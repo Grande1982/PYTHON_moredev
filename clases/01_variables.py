cf1b6492ce4fc0550032fc8c3a8d69a9c68e9b41
Grande1982 <cgrande1982@gmail.com>
2024-01-19 14:58:26 +0100

    dia 1
    dia 1 de python

--- clases/01_variables.py
+++ clases/01_variables.py
@@ -14,10 +14,22 @@ print("mi boolena es: ",mi_boolean)
 #varios argumentos en print
 print(numero,nombre_usuario,mi_boolean)
 
-#cambio de tipo de variable de str a int y de int a str 
-
+#cambio de tipo de variable de sint a str 
 print(type(numero))
-
 numero = str(numero)
 print(type(numero))
+
+#longitud
+print(len(nombre_usuario))
+
+#variable en un linea
+nombre, apellido_primero, apellido_segundo, edad = "Cesar", "Grande", "Conde", 41
+
+print(apellido_segundo)
+print(edad)
+print(nombre)
+print(apellido_primero)
+
+
+print ("mi edad es:",edad, "mi nombre es:", nombre, "y mis apellidos son:",apellido_primero, apellido_segundo)
 
